67fda182de0f579c85efcdb4a2d17aa976978172
Carlos Campirano Angulo <ccampira@asu.edu>
2023-02-13 15:45:00 -0700

    Carlos added a menubar to the program

--- loginGui.py
+++ loginGui.py
@@ -5,8 +5,42 @@ win = Tk()
 win.title('Da Cat')
 win.geometry('400x650')
 win.config(bg='beige')
+
+## ADDED MENUBAR TO PROGRAM -> CARLOS
+menubar = Menu(win)  # add a menu to window object/instance
+
+filemenu = Menu(menubar)  # add File menu and commands
+menubar.add_cascade(label='File', menu=filemenu)
+filemenu.add_command(label='New')
+filemenu.add_command(label='Open')
+filemenu.add_command(label='Save')
+filemenu.add_command(label='Save as')
+filemenu.add_command(label='Close')
+filemenu.add_command(label='Exit', command=win.quit)
+
+# Adding Edit Menu and commands
+edit = Menu(menubar, tearoff=False)  # if tearoff = false then it will be glued to the menu bar.
+# If tearoff = true then it will allow you to move it.
+menubar.add_cascade(label='Edit', menu=edit)
+edit.add_command(label='Cut', command=None)
+edit.add_command(label='Copy', command=None)
+edit.add_command(label='Paste', command=None)
+edit.add_command(label='Select All', command=None)
+edit.add_separator()
+edit.add_command(label='Find...', command=None)
+edit.add_command(label='Find again', command=None)
+
+# Adding Help Menu
+help_ = Menu(menubar, tearoff=0)
+menubar.add_cascade(label='Help', menu=help_)
+help_.add_command(label='Tk Help', command=None)
+help_.add_command(label='Demo', command=None)
+help_.add_separator()
+help_.add_command(label='About Tk', command=None)
+
 canvas = Canvas(win, width=300, height=180, bg='beige')
 canvas.grid(columnspan=3, pady=10)
+
 
 logo = Image.open('thecat.png')
 logo = ImageTk.PhotoImage(logo)
@@ -68,4 +102,5 @@ register_button = Button(order_frame, command=register_click,
                          font=("Arial", 16), text='Register', width=15)
 register_button.grid(column=1, row=10, pady=20, padx=(30, 0), sticky=W)
 
+win.config(menu=menubar)
 win.mainloop()
