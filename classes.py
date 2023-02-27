c81b895ce4cc59f7dcacd96a8f3ab74be296bc82
duhhvonne <124933722+duhhvonne@users.noreply.github.com>
2023-02-27 16:21:25 -0700

    Add files via upload

--- classes.py
+++ classes.py
@@ -3,7 +3,7 @@
 class BookHotel:
 
     # def __init__(self, name, hotel, rooms, guests, addons=0, price=0.00):
-    def __init__(self, name, hotel, rooms, guests, roomtype, price=0.00):
+    def __init__(self, name, hotel, rooms, guests, roomtype, destination, price=0.00):
         # self.name = name
         try:
             fname = name.split()[0]
@@ -16,6 +16,7 @@ class BookHotel:
         self.guests = guests
         self.roomtype = roomtype
         self.price = price
+        self.destination = destination
 
     def custom_confirmation(self):
         return f'{self.name}, your booking is confirmed!'
@@ -23,7 +24,15 @@ class BookHotel:
     def order_details(self):
         return f'Booking: Staying at {self.hotel}, {self.rooms} room(s) with {self.guests} guest(s) with {self.roomtype}'
 
+    def final_confirmation(self):
+        return f'Booking by: {self.name}\n' \
+               f'Destination: {self.destination}\n' \
+               f'{self.hotel}\n' \
+               f'{self.rooms} room(s)\n' \
+               f' {self.guests} guest(s) with {self.roomtype}\n' \
+               f'Total: ${self.price:.2f}'
+
     def __str__(self):
-        return f'{self.hotel}, {self.rooms} room(s), {self.guests} guest(s) with {self.roomtype} ' \
-               f'${self.price:.2f}, Booking by: {self.name}'
+        return f'Booking by: {self.name}, Destination: {self.destination}, {self.hotel}, {self.rooms} room(s), {self.guests} guest(s) with {self.roomtype} ' \
+               f'Total: ${self.price:.2f}'
 
